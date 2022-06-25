f414ae5854374bc1bf8f5b87238daff0914301bb
meghark <98137756+meghark@users.noreply.github.com>
2022-06-24 23:25:55 -0700

    Ad reaction model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,27 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true ,
+        trim: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    }
+});
 
 const ThoughtSchema = new Schema({
     thoughtText:{
@@ -15,7 +37,8 @@ const ThoughtSchema = new Schema({
     username:{
         type: String,
         required: true
-    }
+    },
+    reactions: [ReactionSchema]
     /*
     friends: [{
         //Telling model comments comes from comment model
