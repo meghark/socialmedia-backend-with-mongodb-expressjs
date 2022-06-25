7f52d1cfb67c201a85211dfb6ce8cfef9188f717
meghark <98137756+meghark@users.noreply.github.com>
2022-06-24 20:34:26 -0700

    Add thoughts route

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 
 router.use('/users', userRoute);
-
+router.use('/thoughts', userRoute);
 
 
 module.exports = router;
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getOneThoughtById,
+    addThought,
+    updateThought,
+    removeThought
+} = require("../../controllers/thought-controllers");
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(addThought);
+
+router
+    .route('/:id')
+    .get(getOneThoughtById)
+    .put(updateThought)
+    .delete(removeThought)
+
+
+ module.exports = router;
